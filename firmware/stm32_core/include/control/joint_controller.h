4d2dfc4258af5dd38e189f9a3e9e134efd2c42e4
Chris Alexander Pop <chrisalexanderpop@gmail.com>
2026-03-26 20:47:02 -0400

    Add new driver and test files for encoder and stepper functionality
    - Created encoder_driver_link.cpp to link encoder driver implementation.
    - Created stepper_driver_link.cpp to link stepper driver implementation.
    - Added native_test_app.cpp to include various test files for firmware.
    - Implemented test_driver_scaffolds.cpp to validate stepper and encoder drivers.
    - Developed test_joint_controller.cpp to test joint controller functionality.
    - Introduced test_harness.h for common test utilities.
    - Added packet_codec_link.cpp for packet codec driver linking.
    - Updated snapshots for firmware smoke tests.
    - Included new Python bytecode files for hardware bridge tests.

--- firmware/stm32_core/include/control/joint_controller.h
+++ firmware/stm32_core/include/control/joint_controller.h
@@ -12,6 +12,9 @@ class JointController {
   void UpdateFromSensors(const JointState& measured_state);
   void Step(float dt_s);
 
+  const JointCommand& command() const;
+  const JointState& measured_state() const;
+
  private:
   JointCommand command_{};
   JointState measured_state_{};
--- firmware/stm32_core/include/drivers/encoder_driver.h
+++ firmware/stm32_core/include/drivers/encoder_driver.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include "core/types.h"
+
 namespace robot_arm {
 
 class EncoderDriver {
  public:
   bool Init();
   float ReadJointAngleDeg(int joint_index) const;
+
+  void SetSimulatedJointAngleDegForTest(int joint_index, float angle_deg);
+  bool initialized() const;
+  float simulated_joint_angle_deg(int joint_index) const;
+
+ private:
+  bool initialized_ = false;
+  float simulated_joint_angle_deg_[kJointCount]{};
 };
 
 }  // namespace robot_arm
