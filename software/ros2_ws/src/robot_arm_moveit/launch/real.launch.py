2d7a94c37d1d5c72c92cdf1743d7be40184deb21
Chris Alexander Pop <chrisalexanderpop@gmail.com>
2026-03-26 20:29:38 -0400

    Add MoveIt scaffold and ROS workflow

---
+++ software/ros2_ws/src/robot_arm_moveit/launch/real.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    moveit_share = get_package_share_directory('robot_arm_moveit')
+    description_share = get_package_share_directory('robot_description')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(moveit_share, 'launch', 'moveit.launch.py')
+            )
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(description_share, 'rviz', 'display.rviz')],
+        ),
+    ])
